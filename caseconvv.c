9dfa3a3000271da8fe02ea9f9e19ed46e04d281d
x1x1x5 <activatehim3@gmail.com>
2024-10-18 13:23:29 +0100

    Add files via upload

---
+++ caseconvv.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Enter a letter\n");
+    char letter;
+    scanf("%c",&letter);
+
+    conv(letter);
+}
+
+void conv (letter){
+    switch(letter){
+    //lowercase
+    case 65:
+        letter+=32;
+        printf("%c",letter);
+    case 66:
+        letter+=32;
+        printf("%c",letter);
+    case 67:
+        letter+=32;
+        printf("%c",letter);
+    case 68:
+        letter+=32;
+        printf("%c",letter);
+    case 69:
+        letter+=32;
+        printf("%c",letter);
+    case 70:
+        letter+=32;
+        printf("%c",letter);
+    case 71:
+        letter+=32;
+        printf("%c",letter);
+    case 72:
+        letter+=32;
+        printf("%c",letter);
+    case 73:
+        letter+=32;
+        printf("%c",letter);
+    case 74:
+        letter+=32;
+        printf("%c",letter);
+    case 75:
+        letter+=32;
+        printf("%c",letter);
+    case 76:
+        letter+=32;
+        printf("%c",letter);
+    case 77:
+        letter+=32;
+        printf("%c",letter);
+    case 78:
+        letter+=32;
+        printf("%c",letter);
+    case 79:
+        letter+=32;
+        printf("%c",letter);
+    case 80:
+        letter+=32;
+        printf("%c",letter);
+    case 81:
+        letter+=32;
+        printf("%c",letter);
+    case 82:
+        letter+=32;
+        printf("%c",letter);
+    case 83:
+        letter+=32;
+        printf("%c",letter);
+    case 84:
+        letter+=32;
+        printf("%c",letter);
+    case 85:
+        letter+=32;
+        printf("%c",letter);
+    case 86:
+        letter+=32;
+        printf("%c",letter);
+    case 87:
+        letter+=32;
+        printf("%c",letter);
+    case 88:
+        letter+=32;
+        printf("%c",letter);
+    case 89:
+        letter+=32;
+        printf("%c",letter);
+    case 90:
+        letter+=32;
+        printf("%c",letter);
+
+    //uppercase
+    case 97:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 98:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 99:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 100:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 101:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 102:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 103:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 104:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 105:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 106:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 107:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 108:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 109:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 110:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 111:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 112:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 113:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 114:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 115:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 116:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 117:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 118:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 119:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 120:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 121:
+        letter = letter - 32;
+        printf("%c",letter);
+    case 122:
+        letter = letter - 32;
+        printf("%c",letter);
+
+
+    }
+
+}
